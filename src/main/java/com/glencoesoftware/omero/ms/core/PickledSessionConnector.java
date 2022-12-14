f835fb787f5252efae0e587e941a9e4490869b0f
Chris Allan <callan@glencoesoftware.com>
2022-12-14 22:07:30 +0000

    Add support for LONG_BINPUT offsets

--- src/main/java/com/glencoesoftware/omero/ms/core/PickledSessionConnector.java
+++ src/main/java/com/glencoesoftware/omero/ms/core/PickledSessionConnector.java
@@ -152,6 +152,7 @@ public class PickledSessionConnector implements IConnector {
     private static void assertStoreOpCode(Iterator<Op> opIterator) {
         Op store = opIterator.next();
         if (store.code() != PythonPickle.Opcode.BINPUT
+                && store.code() != PythonPickle.Opcode.LONG_BINPUT
                 && store.code() != PythonPickle.Opcode.MEMOIZE
                 && store.code() != PythonPickle.Opcode.PUT) {
             throw new IllegalArgumentException(
